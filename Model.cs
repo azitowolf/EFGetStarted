8c99ffe139caa9ad4214fba9f4733a70fe1dce42
Alexander Zito-Wolf <azitowolf@Alexanders-MacBook-Pro-2.local>
2020-09-15 08:07:46 -0400

    first commit

---
+++ Model.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<User> Users {get;set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=blogging.db");
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name {get;set;}
+        public int Age {get;set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter an argument.");
+                return 1;
+            } 
+            using (var db = new BloggingContext())
+            {
+                // Create
+                Console.WriteLine("Inserting a new blog=" + args[0]);
+                db.Add(new Blog { Url = args[0] });
+                db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Update
+                Console.WriteLine("Updating the blog and adding a post");
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(
+                    new Post
+                    {
+                        Title = "Hello World",
+                        Content = "I wrote an app using EF Core!"
+                    });
+                db.SaveChanges();
+
+                // Delete
+                // Console.WriteLine("Delete the blog");
+                // db.Remove(blog);
+                // db.SaveChanges();
+                return 1;
+            }
+        }
+    }
+}
